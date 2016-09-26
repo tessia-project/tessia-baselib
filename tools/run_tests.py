2dec1eb9e067b96108567bd6067c4d784551d394
Jose Felipe Santiago Filho <jfelipefilho@br.ibm.com>
2016-09-26 17:59:39 -0300

    Add support to coverage3 on tox
    The previous "test" target was replaced.
    Now, tox will run the coverage command to run the tests and also
     collect code coverage results.
    If a relative path is passed, only the target test will be executed.

--- tools/run_tests.py
+++ tools/run_tests.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 """
-Wrapper script to execute unit tests with unittest lib
+Wrapper script to execute coverage3 on unit tests
 """
 
 #
@@ -27,48 +27,70 @@ import sys
 #
 # CONSTANTS AND DEFINITIONS
 #
-CMD_UNITTEST_DISCOVER = "python3 -m unittest discover -v {} -p '*.py'"
-CMD_UNITTEST_MODULE = "python3 -m unittest -v {}"
-
+CMD_COVERAGE = "python3 -m coverage run -a --source={} -m unittest -v {}"
+CMD_ERASE = "coverage erase"
 #
 # CODE
 #
 def main():
     """
-    Process the command line arguments and create the appropriate unittest
+    Process the command line arguments and create the appropriate coverage3
     command
 
     Args:
-        None
+        Optional    Relative path to test
 
     Returns:
         None
 
     Raises:
-        None
+        Exception   If the operation fails
     """
-    # determine repository's root dir
+
+    # erase previously collected coverage data
+    subprocess.call(CMD_ERASE, shell=True)
+
     my_dir = os.path.dirname(os.path.abspath(__file__))
     lib_dir = os.path.abspath('{}/..'.format(my_dir))
 
-    # no arguments provided: execute all tests
-    if len(sys.argv) < 2:
-        cmd = CMD_UNITTEST_DISCOVER.format('tests/unit')
-    # module path provided: use module's command version
-    elif sys.argv[1].endswith('.py'):
-        cmd = CMD_UNITTEST_MODULE.format(sys.argv[1])
-    # package path provided: use discover option
+    # return value
+    ret = 0
+
+    # module path provided
+    if len(sys.argv) > 1:
+        cmd = CMD_COVERAGE.format(
+            sys.argv[1].replace("tests/unit", "tessia_baselib"),
+            sys.argv[1]
+        )
+        ret += subprocess.call(cmd, shell=True)
     else:
-        cmd = CMD_UNITTEST_DISCOVER.format(sys.argv[1])
+        # search in the directory /tests/unit for all tests
+        for root, dirs, files in os.walk(lib_dir + "/tests/unit"):
+            for name in files:
+                # skip __init__.py files
+                if name.endswith(".py") and name != "__init__.py":
+                    test_abs_path = os.path.join(root, name)
 
-    # switch to root dir to make sure paths are found
-    cmd = 'cd {} && {}'.format(lib_dir, cmd)
+                    test_rel_path = os.path.relpath(test_abs_path, lib_dir)
+                    source = test_abs_path.replace(
+                        "/tests/unit", "/tessia_baselib"
+                    )
 
-    # show command line to user
-    print(cmd)
+                    cmd = CMD_COVERAGE.format(source, test_rel_path)
 
-    # execute and return exit code
-    return subprocess.call(cmd, shell=True)
+                    # show command line to user
+                    print(cmd)
+
+                    cmd = 'cd {} && {}'.format(lib_dir, cmd)
+                    ret += subprocess.call(cmd, shell=True)
+
+    # display report
+    subprocess.call("python3 -m coverage report -m", shell=True)
+
+    if ret != 0:
+        raise Exception(
+            "A tess contain an error or the coverage command failed to run."
+        )
 # main()
 
 if __name__ == '__main__':
