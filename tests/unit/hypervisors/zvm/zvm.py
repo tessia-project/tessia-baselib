2d7a8cff7e7933702013e16289be8c1012247dcf
Willian Rampazzo <willianr@linux.vnet.ibm.com>
2017-02-10 10:27:57 -0200

    Implement login/logoff/stop for HypervisorZvm
    o Implementation of the following methods for HypervisorZvm:
       - login
       - logoff
       - stop
    o Definition of initial jsom schemas for stop and __init__
    o 100% of code coverage on unit tests

---
+++ tests/unit/hypervisors/zvm/zvm.py
@@ -0,0 +1,267 @@
+# Copyright 2017 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Terminal unittest
+"""
+# pylint: disable=no-member,attribute-defined-outside-init
+#
+# IMPORTS
+#
+from unittest.mock import patch
+from unittest import TestCase
+from tessia_baselib.hypervisors.zvm.zvm import HypervisorZvm
+#
+# CONSTANTS AND DEFINITIONS
+#
+
+#
+# CODE
+#
+class TestHypervisorZvm(TestCase):
+    """
+    Unit test for the HypervisorZvm class
+    """
+
+    def setUp(self):
+        """
+        Mock Terminal
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError: if the session object does not behave as expected
+        """
+        self.terminal_patcher = patch(
+            'tessia_baselib.hypervisors.zvm.zvm.Terminal',
+            autospec=True)
+        self.mock_terminal = self.terminal_patcher.start()
+        self.addCleanup(self.terminal_patcher.stop)
+
+        # set s3270 output
+        self.mock_terminal.return_value.login.return_value = 'ok\n'
+    # setUp()
+
+    def test_login_ok(self):
+        """
+        Exercise a normal login command
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError: if the session object does not behave as expected
+        """
+        # create new instance of terminal
+        hyp = HypervisorZvm(
+            "hostname",
+            "hostname.com",
+            "user",
+            "password",
+            None
+        )
+
+        # simple command execution
+        hyp.login()
+        self.mock_terminal.return_value.login.assert_called_once_with(
+            "hostname.com",
+            "user",
+            "password",
+            {},
+            60
+        )
+    # test_login_ok()
+
+    def test_logoff_ok(self):
+        """
+        Exercise a normal logoff command
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError: if the session object does not behave as expected
+        """
+        # create new instance of terminal
+        hyp = HypervisorZvm(
+            "hostname",
+            "hostname.com",
+            "user",
+            "password",
+            None
+        )
+
+        # simple command execution
+        hyp.login()
+        hyp.logoff()
+        self.mock_terminal.return_value.logoff.assert_called_once_with()
+    # test_logoff_ok()
+
+    def test_logoff_failed(self):
+        """
+        Exercise a failed logoff command
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError: if the session object does not behave as expected
+        """
+        self.mock_terminal.return_value.logoff.return_value = False
+        # create new instance of terminal
+        hyp = HypervisorZvm(
+            "hostname",
+            "hostname.com",
+            "user",
+            "password",
+            None
+        )
+
+        # simple command execution
+        hyp.login()
+        self.assertRaises(RuntimeError, hyp.logoff)
+    # test_logoff_failed()
+
+    def test_start_ok(self):
+        """
+        Exercise a normal start command
+
+        *** Placeholder for start test when implemented ***
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError: if the session object does not behave as expected
+        """
+        # create new instance of terminal
+        hyp = HypervisorZvm(
+            "hostname",
+            "hostname.com",
+            "user",
+            "password",
+            None
+        )
+
+        # simple command execution
+        hyp.login()
+        self.assertRaises(NotImplementedError, hyp.start,
+                          "hostname", "cpu", "memory", None)
+    # test_start_ok()
+
+    def test_stop_ok(self):
+        """
+        Exercise a normal stop command
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError: if the session object does not behave as expected
+        """
+        # create new instance of terminal
+        hyp = HypervisorZvm(
+            "hostname",
+            "hostname.com",
+            "user",
+            "password",
+            None
+        )
+
+        # simple command execution
+        hyp.login()
+        hyp.stop("hostname", None)
+        self.mock_terminal.return_value.logoff.assert_called_once_with(
+            parameters={'logoff': True})
+    # test_stop_ok()
+
+    def test_stop_failed(self):
+        """
+        Exercise a failed stop command
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError: if the session object does not behave as expected
+        """
+        self.mock_terminal.return_value.logoff.return_value = False
+        # create new instance of terminal
+        hyp = HypervisorZvm(
+            "hostname",
+            "hostname.com",
+            "user",
+            "password",
+            None
+        )
+
+        # simple command execution
+        hyp.login()
+        self.assertRaises(RuntimeError, hyp.stop,
+                          "hostname", None)
+    # test_stop_failed()
+
+    def test_reboot_ok(self):
+        """
+        Exercise a normal reboot command
+
+        *** Placeholder for reboot test when implemented ***
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError: if the session object does not behave as expected
+        """
+        # create new instance of terminal
+        hyp = HypervisorZvm(
+            "hostname",
+            "hostname.com",
+            "user",
+            "password",
+            None
+        )
+
+        # simple command execution
+        hyp.login()
+        self.assertRaises(NotImplementedError, hyp.reboot,
+                          "hostname", None)
+    # test_reboot_ok()
+
+# TestHypervisorZvm
