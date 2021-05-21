3df89c68a5a91030f6843f33a2199842f7c30067
Alexander Efremkin <aefr@de.ibm.com>
2021-05-21 18:00:42 +0200

    Reconnect to OS messages when unavailable
    When JMS error occurs that prevents communicating with OS messages, HypervisorHmc now can reconnect to the OS messages interface.

--- tessia/baselib/hypervisors/hmc/messages.py
+++ tessia/baselib/hypervisors/hmc/messages.py
@@ -21,7 +21,7 @@ Implementation of Operating System Messages reader
 #
 from queue import Empty, Queue
 from threading import Thread
-from zhmcclient import NotificationReceiver
+from zhmcclient import NotificationReceiver, NotificationJMSError
 
 #
 # CONSTANTS
@@ -31,6 +31,12 @@ NOTIFICATION_TYPE_MSG_FIELDS = {
 }
 
 # Internal queue length
+# Queue accumulates messages from NotificationReceiver
+# and blocks when length is exceeded.
+# Each queue entry is not a single message, but a chunk of several messages,
+# as received by NotificationReceiver.
+# The exact value is therefore somewhat arbitrary chosen, but value of 1 will
+# serialize processing/waiting loop
 MAX_QUEUED_NOTIFICATIONS = 20
 
 
@@ -42,16 +48,17 @@ class Messages:
     and buffered until requested with get_messages()
     """
 
-    def __init__(self, notification_receiver, notification_type='os-message'):
+    def __init__(self, reconnect_function, notification_type='os-message'):
         """
         Create an async reader from notifications channel
 
         Args:
-            notification_receiver (zhmcclient.NotificationReceiver):
-                an active NotificationReceiver object
+            reconnect_function (Callable[[], zhmcclient.NotificationReceiver]):
+                a function returning a NotificationReceiver object
             notification_type (str): type of messages to process
         """
-        self.channel = notification_receiver
+        self._reconnect = reconnect_function
+        self.channel = self._reconnect()
         self.notification_type = notification_type
         self.msg_field = NOTIFICATION_TYPE_MSG_FIELDS[self.notification_type]
         self.received_msg_queue = Queue(MAX_QUEUED_NOTIFICATIONS)
@@ -72,7 +79,8 @@ class Messages:
         Having context assures that this object owns communication channel
         and will close it after it is no longer needed
         """
-        self.channel.close()
+        if self.channel:
+            self.channel.close()
         if self.poll_thread:
             self.poll_thread.join(timeout=5.0)
     # __exit__()
@@ -81,23 +89,54 @@ class Messages:
     def connect(cls, channel_topic, hmc, user, passwd):
         """
         Create Messages instance from parameters
+
+        Args:
+            channel_topic (Union[str, Callable]): channel topic to listen to.
+                May be a callable in case topic changes
+            hmc (str): HMC to connnect to
+            user (str): username for HMC
+            passwd (str): user password for HMC
+
+        Returns:
+            Messages: Messages instance
         """
-        return cls(NotificationReceiver(channel_topic, hmc, user, passwd))
+        def _reconnect():
+            if callable(channel_topic):
+                topic = channel_topic()
+            else:
+                topic = channel_topic
+
+            return NotificationReceiver(topic, hmc, user, passwd)
+
+        return cls(_reconnect)
 
     def _get_notification(self):
         """
-        Retrieve a single HMC notification, which may contain several messages
+        Retrieve pending HMC notifications, which may contain several messages
 
         Reading notifications is a blocking procedure and stalls an event loop,
         so this method should be run in a separate thread
         """
-        for headers, message in self.channel.notifications():
-            if headers['notification-type'] != self.notification_type:
-                continue
+        if not self.channel:
+            return
 
-            messages = [entry['message-text'].strip('\n')
-                        for entry in message[self.msg_field]]
-            self.received_msg_queue.put(messages)
+        try:
+            for headers, message in self.channel.notifications():
+                if headers['notification-type'] != self.notification_type:
+                    continue
+
+                messages = [{
+                    'type': 'OS message',
+                    'text': entry['message-text'].strip('\n')}
+                    for entry in message[self.msg_field]]
+                self.received_msg_queue.put(messages)
+        except NotificationJMSError as jms_error:
+            # there is an error raised, and connection is somewhat broken
+            self.received_msg_queue.put([{
+                'type': 'JMS error',
+                'text': "{}".format(jms_error)}])
+            self.channel = None
+
     # _get_notification()
 
     def get_messages(self, timeout=5.0):
@@ -111,6 +150,11 @@ class Messages:
         Returns:
             List: messages
         """
+        if not self.channel:
+            # we are calling reconnect, which may raise, and we want exceptions
+            # to be propagated in caller thread, not in our separete listener
+            self.channel = self._reconnect()
+
         if not self.poll_thread or not self.poll_thread.is_alive():
             self.poll_thread = Thread(
                 name="hmc-listener", target=self._get_notification)
